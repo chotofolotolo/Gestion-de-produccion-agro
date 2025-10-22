994c035dc3358422192eda1bec92973af50e20d2
Dylan Balvidares <Dylanaza037@gmail.com>
2025-10-21 23:38:27 -0300

    cambios minimos

--- ProduccionAgropecuaria.java
+++ ProduccionAgropecuaria.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class produccionAgropecuaria {
+public class ProduccionAgropecuaria {
 
 	public static void main(String[] args) {
-		SpringApplication.run(produccionAgropecuaria.class, args);
+		SpringApplication.run(ProduccionAgropecuaria.class, args);
 
 	}
 
